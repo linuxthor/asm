3ace1545acc51d4a629a9ef403a1dc041396494a
linuxthor <linuxthor@users.noreply.github.com>
2019-02-17 22:02:34 +0000

    Add files via upload

--- libssh.asm
+++ libssh.asm
@@ -1,6 +1,6 @@
 ; linuxthor
 ;
-; simple libssh example 
+; simple libssh example for passwd auth 
 ;
 ; assemble with:
 ; nasm -f elf64 -o libssh.o libssh.asm
@@ -10,9 +10,10 @@
 BITS 64
 
 extern ssh_options_set, ssh_new, ssh_connect, ssh_disconnect
-extern ssh_free
+extern ssh_free, ssh_userauth_password
 
 %define SSH_OPTIONS_HOST 0
+%define SSH_OPTIONS_USER 4
 
 global main
 
@@ -34,8 +35,24 @@ main:
 
     push rbp
     mov  rdi, [ssh_sesh]
+    mov  rsi, SSH_OPTIONS_USER       
+    mov  rdx, usr                    
+    xor  rax, rax
+    call ssh_options_set
+    pop  rbp 
+
+    push rbp
+    mov  rdi, [ssh_sesh]
     xor  rax, rax
     call ssh_connect 
+    pop  rbp
+
+    push rbp
+    mov  rdi, [ssh_sesh]
+    mov  rsi, 0
+    mov  rdx, pwd
+    xor  rax, rax
+    call ssh_userauth_password
     pop  rbp
 
     push rbp
@@ -54,7 +71,9 @@ main:
     ret 
 
 section .data
-    con db '192.168.0.1',0  
+    con db '192.168.0.32',0  
+    usr db 'username',0
+    pwd db '!passwd!',0
 
 section .bss
     ssh_sesh resq 1
