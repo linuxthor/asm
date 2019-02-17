69bf616ccd81b1ba35d2d442ea29c2f12ab9fb4a
linuxthor <linuxthor@users.noreply.github.com>
2019-02-17 08:20:41 +0000

    Add files via upload

---
+++ curl.asm
@@ -0,0 +1,63 @@
+;
+; simple libcurl example 
+;
+; assemble with:
+; nasm -f elf64 -o curly.o curly.asm
+; gcc curly.o -no-pie -o curly -lcurl
+;
+
+BITS 64
+
+extern curl_global_init, curl_easy_init, curl_easy_perform
+extern curl_easy_setopt, curl_easy_cleanup, curl_global_cleanup
+
+%define CURLOPT_URL         10002
+%define CURL_GLOBAL_DEFAULT 3
+
+global main
+
+main: 
+    push rbp
+    mov rdi, CURL_GLOBAL_DEFAULT       ; CURL_GLOBAL_DEFAULT
+    xor eax, eax              
+    call curl_global_init
+    pop rbp
+
+    push rbp
+    call curl_easy_init
+    pop rbp
+
+    mov  [curly], rax
+
+    push rbp
+    mov rdi, [curly]
+    mov rsi, CURLOPT_URL                ; CURLOPT_URL
+    mov rdx, url
+    xor rax, rax
+    call curl_easy_setopt
+    pop rbp    
+
+    push rbp
+    mov rdi, [curly]
+    xor eax, eax 
+    call curl_easy_perform
+    pop rbp 
+
+    push rbp 
+    mov rdi, [curly]
+    xor eax, eax
+    call curl_easy_cleanup
+    pop rbp
+
+    push rbp
+    call curl_global_cleanup
+    pop rbp
+
+    xor eax, eax         
+    ret 
+
+section .data
+    url db 'http://www.example.com/',0  
+
+section .bss
+    curly resq 1
