62f15b4787b5f4f03b7c34a13cb7a52704b1aa08
linuxthor <linuxthor@users.noreply.github.com>
2019-02-18 19:44:10 +0000

    Add files via upload

--- libssh.asm
+++ libssh.asm
@@ -21,73 +21,65 @@ global main
 
 main: 
     push rbp
+    mov  rbp, rsp
     xor  eax, eax              
     call ssh_new
-    pop  rbp
 
     cmp rax, 0
     je  error
 
     mov [ssh_sesh], rax
 
-    push rbp
     mov  rdi, [ssh_sesh]
     mov  rsi, SSH_OPTIONS_HOST
     mov  rdx, con
     xor  rax, rax
     call ssh_options_set
-    pop  rbp 
 
     cmp  rax, 0
     jne  error
 
-    push rbp
     mov  rdi, [ssh_sesh]
     mov  rsi, SSH_OPTIONS_USER       
     mov  rdx, usr                    
     xor  rax, rax
     call ssh_options_set
-    pop  rbp 
 
     cmp  rax, 0
     jne  error
 
-    push rbp
     mov  rdi, [ssh_sesh]
     xor  rax, rax
     call ssh_connect 
-    pop  rbp
 
     cmp  rax, SSH_OK
     jne  error
 
-    push rbp
     mov  rdi, [ssh_sesh]
     mov  rsi, 0
     mov  rdx, pwd
     xor  rax, rax
     call ssh_userauth_password
-    pop  rbp
 
     cmp  rax, SSH_AUTH_SUCCESS                
     jne  error
 
-    push rbp
     mov  rdi, [ssh_sesh]
     xor  rax, rax
     call ssh_disconnect 
-    pop  rbp
 
-    push rbp
     mov  rdi, [ssh_sesh]
     xor  rax, rax
     call ssh_free
-    pop  rbp
 
+    mov rsp, rbp
+    pop rbp
     xor eax, eax         
     ret 
 
 error:
+    mov rsp, rbp
+    pop rbp
     mov rax, 1
     ret
 
