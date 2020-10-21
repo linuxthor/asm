9a877e9bf7a6a2dbafb8868858365d88512b96dc
linuxthor <linuxthor@users.noreply.github.com>
2020-10-21 23:27:10 +0100

    Update sys_mmap.asm

--- syscalls/linux/009_sys_mmap/sys_mmap.asm
+++ syscalls/linux/009_sys_mmap/sys_mmap.asm
@@ -20,8 +20,9 @@ BITS 64
 %define PROT_NONE    0x00
 
 ; must have one of..
-%define MAP_SHARED   0x01
-%define MAP_PRIVATE  0x02
+%define MAP_SHARED    0x01
+%define MAP_PRIVATE   0x02
+%define MAP_ANONYMOUS 0x20
 
 ; can OR one or more of..
 %define MAP_FIXED               0x10		
